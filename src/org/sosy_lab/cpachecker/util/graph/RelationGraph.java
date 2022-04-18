3e5823b0c41833620aa095ab8eb75adff5f7f8e9
fanweneddie <1164087609@qq.com>
2022-04-18 20:35:36 +0800

    try to call the defined methods internally; and fix the comments to make their format unified; and mark deprecated annotation for some methods that we don't use; and write some methods such as adjacentEdges(), edgesConnecting() and hasEdgeConnecting()

--- src/org/sosy_lab/cpachecker/util/graph/RelationGraph.java
+++ src/org/sosy_lab/cpachecker/util/graph/RelationGraph.java
@@ -17,7 +17,6 @@ import com.google.common.graph.ElementOrder;
 import com.google.common.graph.EndpointPair;
 import com.google.common.graph.Graph;
 import com.google.common.graph.MutableNetwork;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
@@ -90,8 +89,8 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
      * <p></p>
      * @param pN the starting node, which must not be null and must have been in graph.
      * @param pN1 the ending node, which must not be null and must have been in graph.
-     * @param pRE the newly added relation edge, which must not be null
-     *             and valid and must connect <code>pN</code> to <code>pN1</code>.
+     * @param pRE the newly added relation edge, which must not be null,
+     *             and must be valid and must connect <code>pN</code> to <code>pN1</code>.
      * @return true if the graph is modified.
      */
     @Override
@@ -102,7 +101,7 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
         assert containsNode(pN) && containsNode(pN1);
         assert pRE.valid() && pRE.connects(pN, pN1);
 
-        if (!adjStartList.get(pN).contains(pRE)) {
+        if (!containsEdge(pRE)) {
             adjStartList.get(pN).add(pRE);
             adjEndList.get(pN1).add(pRE);
             return true;
@@ -112,13 +111,13 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
     }
 
     /**
-     * Add a directed edge from one node to the other in the given <code>EndpointPair</code>,
+     * Add a directed relation edge from one node to the other in the given pair of nodes,
      * if the edge is not present.
-     * We just call <code>addEdge(N pN, N pN1, E pE)</code> to solve the problem.
-     * <p></p>
-     * @param pEndpointPair a pair of nodes, whose first element is starting node
+     * We just call {@link RelationGraph#addEdge(Object, Object, RelationEdge)} to solve the problem.
+     * @param pEndpointPair the given pair of nodes, whose first element is starting node
      *                     and second element is ending point. It must not be null.
-     * @param pRE the newly added edge.
+     * @param pRE the newly added relation edge, which must not be null,
+     *            and must be valid and must connect two nodes in <code>pEndpointPair</code>.
      * @return true if the graph is modified.
      */
     @Override
@@ -142,15 +141,12 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
         assertNotNull(pN);
 
         if (containsNode(pN)) {
-            // remove edges that start from pN
             for (RE relationEdge : adjStartList.get(pN)) {
                 removeEdge(relationEdge);
             }
-            // remove edges that ends at pN
             for (RE relationEdge : adjEndList.get(pN)) {
                 removeEdge(relationEdge);
             }
-            // remove the node
             adjStartList.remove(pN);
             adjEndList.remove(pN);
             return true;
@@ -165,7 +161,7 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
      * <p></p>
      * Note that <code>adjStartList</code> and <code>adjEndList</code> should have consistent nodes and relation edges.
      * <p></p>
-     * @param pRE the edge to be removed, which must not be null.
+     * @param pRE the relation edge to be removed, which must not be null and must be valid.
      * @return true if the graph is modified.
      */
     @Override
@@ -175,8 +171,7 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
 
         N startNode = pRE.getStartNode();
         N endNode = pRE.getEndNode();
-        if (containsNode(startNode)
-            && adjStartList.get(startNode).contains(pRE)) {
+        if (containsEdge(pRE)) {
             adjStartList.get(startNode).remove(pRE);
             adjEndList.get(endNode).remove(pRE);
             return true;
@@ -212,6 +207,7 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
     /**
      * We don't use this method.
      */
+    @Deprecated
     @Override
     public Graph<N> asGraph() {
         return null;
@@ -244,6 +240,7 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
     /**
      * We don't use this method.
      */
+    @Deprecated
     @Override
     public ElementOrder<N> nodeOrder() {
         return null;
@@ -252,6 +249,7 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
     /**
      * We don't use this method.
      */
+    @Deprecated
     @Override
     public ElementOrder<RE> edgeOrder() {
         return null;
@@ -262,7 +260,6 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
      * We just call <code>predecessors()</code> and <code>successors()</code> to solve the problem.
      * @param pN the given node, which must not be null and must be in this graph.
      * @return the set of adjacent nodes of <code>pN</code>.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
      */
     @Override
     public Set<N> adjacentNodes(N pN) {
@@ -274,104 +271,95 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
     }
 
     /**
-     * Get the predecessors of a given node.
+     * Get the predecessors of a given node, i.e. the nodes that point to the given node.
      * @param pN the given node, which must not be null and must be in the graph.
-     * @return the set of nodes that point to the given node.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
+     * @return the set of predecessors of <code>pN</code>.
      */
     @Override
     public Set<N> predecessors(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
-        // get the nodes that point to pN
-        if (containsNode(pN)) {
-            Set<N> adjNodes = new HashSet<>();
-            for (RE relationEdge : adjEndList.get(pN)) {
-                adjNodes.add(relationEdge.getStartNode());
-            }
-            return adjNodes;
-        } else {
-            throw new IllegalArgumentException("node must be in the graph");
+        Set<N> adjNodes = new HashSet<>();
+        for (RE relationEdge : inEdges(pN)) {
+            adjNodes.add(relationEdge.getStartNode());
         }
+        return adjNodes;
     }
 
     /**
-     * Get the successors of a given node.
+     * Get the successors of a given node, i.e. the nodes that the given node points to.
      * @param pN the given node, which must not be null and must be in this graph.
-     * @return the set of nodes that the given node points to.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
+     * @return the set of successors of <code>pN</code>.
      */
     @Override
     public Set<N> successors(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
-        // get the nodes that pN points to
-        if (containsNode(pN)) {
-            Set<N> adjNodes = new HashSet<>();
-            for (RE relationEdge : adjStartList.get(pN)) {
-                adjNodes.add(relationEdge.getEndNode());
-            }
-            return adjNodes;
-        } else {
-            throw new IllegalArgumentException("node must be in the graph");
+        Set<N> adjNodes = new HashSet<>();
+        for (RE relationEdge : outEdges(pN)) {
+            adjNodes.add(relationEdge.getEndNode());
         }
+        return adjNodes;
     }
 
+    /**
+     * Get the relation edges that a given node connects with.
+     * @param pN the given node, which must not be null and must be in this graph.
+     * @return the union set of {@link RelationGraph#inEdges} and {@link RelationGraph#outEdges}.
+     */
     @Override
     public Set<RE> incidentEdges(N pN) {
-        return null;
+        assertNotNull(pN);
+        assert containsNode(pN);
+
+        Set<RE> incidentEdges = new HashSet<>();
+        incidentEdges.addAll(inEdges(pN));
+        incidentEdges.addAll(outEdges(pN));
+        return incidentEdges;
     }
 
     /**
      * Get the relation edges that end at a given node.
      * @param pN the given node, which must not be null and must be in this graph.
      * @return the set of in-edges of <code>pN</code>.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
      */
     @Override
     public Set<RE> inEdges(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
-        // get the relation edges that ends at pN
-        if (containsNode(pN)) {
-            Set<RE> inEdges = new HashSet<>();
-            inEdges.addAll(adjEndList.get(pN));
-            return inEdges;
-        } else {
-            throw new IllegalArgumentException("node must be in the graph");
-        }
+        Set<RE> inEdges = new HashSet<>();
+        inEdges.addAll(adjEndList.get(pN));
+        return inEdges;
     }
 
     /**
      * Get the relation edges that start from a given node.
      * @param pN the given node, which must not be null and must be in this graph.
      * @return the set of out-edges of <code>pN</code>.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
      */
     @Override
     public Set<RE> outEdges(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
-        // get the relation edges that start from pN
-        if (containsNode(pN)) {
-            Set<RE> outEdges = new HashSet<>();
-            outEdges.addAll(adjStartList.get(pN));
-            return outEdges;
-        } else {
-            throw new IllegalArgumentException("node must be in the graph");
-        }
+        Set<RE> outEdges = new HashSet<>();
+        outEdges.addAll(adjStartList.get(pN));
+        return outEdges;
     }
 
     /**
-     * Count the degree of a given node, i.e. in-degree + out-degree,
+     * Count the degree of a given node, which equals to in-degree + out-degree,
      * where self loop is counted twice.
      * @param pN the given node, which must not be null and must be in this graph.
      * @return the degree of <code>pN</code>.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
      */
     @Override
     public int degree(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
         return inDegree(pN) + outDegree(pN);
     }
@@ -380,100 +368,204 @@ public class RelationGraph<N, L, RE extends RelationEdge<N, L>>
      * Count the in-degree of a given node, i.e. the number of edges that end at the node.
      * @param pN the given node, which must not be null and must be in this graph.
      * @return the in-degree of <code>pN</code>.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
      */
     @Override
     public int inDegree(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
         return inEdges(pN).size();
     }
 
     /**
-     * Count the out-degree of a given node, i.e. the number of edges that start from the node
+     * Count the out-degree of a given node, i.e. the number of edges that start from the node.
      * @param pN the given node, which must not be null and must be in this graph.
      * @return the out-degree of <code>pN</code>.
-     * @throws <code>IllegalArgumentException</code> if <code>pN</code> is not in the graph.
      */
     @Override
     public int outDegree(N pN) {
         assertNotNull(pN);
+        assert containsNode(pN);
 
         return outEdges(pN).size();
     }
 
+    /**
+     * Get the nodes that connect with a given relation edge.
+     * <p></p>
+     * We don't use this method, since we can simply call {@link RelationEdge#getStartNode}
+     * and {@link RelationEdge#getEndNode}.
+     */
+    @Deprecated
     @Override
     public EndpointPair<N> incidentNodes(RE pRE) {
         return null;
     }
 
+    /**
+     * Get the relation edges that are adjacent to the given relation edge,
+     * i.e. they share at least a common node.
+     * <p></p>
+     * Note that a relation edge is not adjacent to itself.
+     * @param pRE the given relation edge, which must not be null and must be valid,
+     *            and must be in the graph.
+     * @return the set of adjacent edges of <code>pRE</code>.
+     */
     @Override
-    public Set<RE> adjacentEdges(RE pE) {
-        return null;
+    public Set<RE> adjacentEdges(RE pRE) {
+        assertNotNull(pRE);
+        assert pRE.valid();
+        assert containsEdge(pRE);
+
+        N startNode = pRE.getStartNode();
+        N endNode = pRE.getEndNode();
+
+        Set<RE> adjEdges = new HashSet<>();
+        adjEdges.addAll(incidentEdges(startNode));
+        adjEdges.addAll(incidentEdges(endNode));
+        adjEdges.remove(pRE);
+        return adjEdges;
     }
 
+    /**
+     * Get the relation edges that start from one given node and end at the other given node.
+     * @param pN the expected starting node, which must not be null and must be in this graph.
+     * @param pN1 the expected ending node, which must not be null and must be in this graph.
+     * @return the set of relation edges from <code>pN</code> to <code>pN1</code>.
+     */
     @Override
     public Set<RE> edgesConnecting(N pN, N pN1) {
-        return null;
+        assertNotNull(pN);
+        assertNotNull(pN1);
+        assert containsNode(pN) && containsNode(pN);
+
+        Set<RE> edges = new HashSet<>();
+        for (RE outEdge : outEdges(pN)) {
+            if (outEdge.endsAt(pN1)) {
+                edges.add(outEdge);
+            }
+        }
+        return edges;
     }
 
+    /**
+     * Get the relation edges that start from the first node and
+     * end at the second node of the given pair of nodes.
+     * <p></p>
+     * We just call {@link RelationGraph#edgesConnecting(Object, Object)} to solve this problem.
+     * @param pEndpointPair the given pair of nodes, whose first element is starting node
+     *                      and second element is ending point. It must not be null.
+     * @return the set of relation edges from <code>pEndpointPair.nodeU()</code>
+     *         to <code>pEndpointPair.nodeV()</code>.
+     */
     @Override
     public Set<RE> edgesConnecting(EndpointPair<N> pEndpointPair) {
-        return null;
+        assertNotNull(pEndpointPair);
+
+        return edgesConnecting(pEndpointPair.nodeU(), pEndpointPair.nodeV());
     }
 
+    /**
+     * We don't use this method.
+     * Please use {@link RelationGraph#edgesConnecting(Object, Object)}.
+     */
+    @Deprecated
     @Override
     public Optional<RE> edgeConnecting(N pN, N pN1) {
         return Optional.empty();
     }
 
+    /**
+     * We don't use this method.
+     * Please use {@link RelationGraph#edgesConnecting(EndpointPair)}.
+     */
+    @Deprecated
     @Override
     public Optional<RE> edgeConnecting(EndpointPair<N> pEndpointPair) {
         return Optional.empty();
     }
 
+    /**
+     * We don't use this method.
+     * Please use {@link RelationGraph#edgesConnecting(Object, Object)}.
+     */
+    @Deprecated
     @Nullable
     @Override
     public RE edgeConnectingOrNull(N pN, N pN1) {
         return null;
     }
 
+    /**
+     * We don't use this method.
+     * Please use {@link RelationGraph#edgesConnecting(EndpointPair)}.
+     */
+    @Deprecated
     @Nullable
     @Override
     public RE edgeConnectingOrNull(EndpointPair<N> pEndpointPair) {
         return null;
     }
 
+    /**
+     * Check whether there is any relation edge that starts from one given node and
+     * ends at the other given node.
+     * @param pN the expected starting node, which must not be null and must be in this graph.
+     * @param pN1 the expected ending node, which must not be null and must be in this graph.
+     * @return true if there exist a relation edge from <code>pN</code> to <code>pN1</code>.
+     */
     @Override
     public boolean hasEdgeConnecting(N pN, N pN1) {
-        return false;
+       assertNotNull(pN);
+       assertNotNull(pN1);
+       assert containsNode(pN) && containsNode(pN1);
+
+       return edgesConnecting(pN, pN1).size() > 0;
     }
 
+    /**
+     * Check whether there is any relation edge that starts from the first node and
+     * ends at the second node of the given pair of nodes.
+     * <p></p>
+     * We just call {@link RelationGraph#hasEdgeConnecting(Object, Object)} to solve this problem.
+     * @param pEndpointPair the given pair of nodes, whose first element is starting node
+     *                      and second element is ending point. It must not be null.
+     * @return true if there exist a relation edge from <code>pEndpointPair.nodeU()</code>
+     *         to <code>pEndpointPair.nodeV()</code>.
+     */
     @Override
     public boolean hasEdgeConnecting(EndpointPair<N> pEndpointPair) {
-        return false;
+        assertNotNull(pEndpointPair);
+
+        return hasEdgeConnecting(pEndpointPair.nodeU(), pEndpointPair.nodeV());
     }
 
     /**
      * Check whether this graph contains a given node.
-     * @param pN the given node, which must not be null
-     * @return true if this graph contains the given node.
+     * @param pN the given node, which must not be null.
+     * @return true if this graph contains <code>pN</code>.
      */
     public boolean containsNode(N pN) {
         assertNotNull(pN);
 
-        return nodes().contains(pN);
+        return adjStartList.containsKey(pN);
     }
 
     /**
      * Check whether this graph contains a given relation edge.
      * @param pRE the given relation edge, which must not be null and must be valid.
-     * @return true if this graph contains the given relation edge
+     * @return true if this graph contains <code>pRE</code>.
      */
     public boolean containsEdge(RE pRE) {
         assertNotNull(pRE);
         assert pRE.valid();
 
-        return edges().contains(pRE);
+        N startNode = pRE.getStartNode();
+        if (adjStartList.containsKey(startNode)
+            && adjStartList.get(startNode).contains(pRE)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
