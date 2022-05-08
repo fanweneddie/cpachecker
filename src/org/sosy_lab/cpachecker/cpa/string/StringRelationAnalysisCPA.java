77587d5843c1bd7c38554330bd2b138dcb17fc48
fanweneddie <1164087609@qq.com>
2022-05-08 09:39:05 +0800

    incorporate StringRelationAnalysisCPA into CompositeCPA

--- src/org/sosy_lab/cpachecker/cpa/string/StringRelationAnalysisCPA.java
+++ src/org/sosy_lab/cpachecker/cpa/string/StringRelationAnalysisCPA.java
@@ -11,40 +11,72 @@ SPDX-License-Identifier: Apache-2.0
 
 package org.sosy_lab.cpachecker.cpa.string;
 
-import org.sosy_lab.common.ShutdownNotifier;
 import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.configuration.InvalidConfigurationException;
+import org.sosy_lab.common.configuration.Option;
 import org.sosy_lab.common.configuration.Options;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.core.defaults.AbstractCPA;
+import org.sosy_lab.cpachecker.core.defaults.AutomaticCPAFactory;
+import org.sosy_lab.cpachecker.core.defaults.DelegateAbstractDomain;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.CPAFactory;
+import org.sosy_lab.cpachecker.core.interfaces.MergeOperator;
 import org.sosy_lab.cpachecker.core.interfaces.StateSpacePartition;
-import org.sosy_lab.cpachecker.cpa.string.StringRelationAnalysisState.StringRelationLabel;
-import org.sosy_lab.cpachecker.util.graph.RelationEdge;
-import org.sosy_lab.cpachecker.util.graph.RelationGraph;
-import org.sosy_lab.cpachecker.util.states.MemoryLocation;
+import org.sosy_lab.cpachecker.core.interfaces.StopOperator;
+import org.sosy_lab.cpachecker.cpa.value.ValueAnalysisState;
 
 @Options(prefix = "cpa.string")
 public class StringRelationAnalysisCPA extends AbstractCPA {
+
+    @Option(secure=true, name="merge", toUppercase=true, values={"SEP", "JOIN"},
+        description="which merge operator to use for StringRelationAnalysisCPA")
+    private final String mergeType = "SEP";
+
+    @Option(
+        secure = true,
+        name = "stop",
+        toUppercase = true,
+        values = {"SEP", "JOIN", "NEVER", "EQUALS"},
+        description = "which stop operator to use for StringRelationAnalysisCPA")
+    private final String stopType = "SEP";
+
     private final Configuration config;
     private final LogManager logger;
-    private final ShutdownNotifier shutdownNotifier;
     private final CFA cfa;
 
-    public StringRelationAnalysisCPA(Configuration pConfig, LogManager pLogger,
-                                     ShutdownNotifier pShutdownNotifier, CFA pCfa) {
-        // Todo: create abstract domain for super class
-        super(null, null);
+    public StringRelationAnalysisCPA(Configuration pConfig,
+                                     LogManager pLogger,
+                                     CFA cCfa) throws InvalidConfigurationException  {
+        super(DelegateAbstractDomain.<ValueAnalysisState>getInstance(), null);
         this.config           = pConfig;
         this.logger           = pLogger;
-        this.shutdownNotifier = pShutdownNotifier;
-        this.cfa              = pCfa;
+        this.cfa              = cCfa;
+    }
+
+    public static CPAFactory factory() {
+        return AutomaticCPAFactory.forType(StringRelationAnalysisCPA.class);
+    }
+
+    @Override
+    public MergeOperator getMergeOperator() {
+        return buildMergeOperator(mergeType);
+    }
+
+    @Override
+    public StopOperator getStopOperator() {
+        return buildStopOperator(stopType);
+    }
+
+    @Override
+    public StringRelationAnalysisTransferRelation getTransferRelation() {
+        return new StringRelationAnalysisTransferRelation(null, null, null);
     }
 
     @Override
     public AbstractState getInitialState(CFANode pNode, StateSpacePartition pPartition) {
-        return new StringRelationAnalysisState(
-            (RelationGraph<MemoryLocation, StringRelationLabel, RelationEdge<MemoryLocation, StringRelationLabel>>) null);
+        return new StringRelationAnalysisState();
     }
 }
